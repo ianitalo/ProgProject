03fe6dec78c5395df3c26d8d994ec6e56dc64a9a
Fábio Teixeira <fabioerubenteixeira@gmail.com>
2021-04-08 11:23:30 +0100

    Update Origem.cpp

--- ProgProj/ProgProj/Origem.cpp
+++ ProgProj/ProgProj/Origem.cpp
@@ -58,21 +58,30 @@ void menu()
 	cin >> answer;
 	if (answer == 1)
 	{
-		cout << "You will be placed in a maze made up of high-voltage fences." << endl <<
-			"There are robots that will try to destroy you." << endl <<
-			"The only way you have to destroy them (and win the game) " << endl <<
-			"is to make them crash with each other or make them crash" << endl <<
-			"with the eletric fences (sorry, you can not punch them)." << endl <<
-			"If you are the one hiting the robots or colliding with the fences you die." << endl <<
-			"Every time you make a move the robots will also make a move going to your direction by the shortest path" << endl <<
-			"(thats how you trick them). The robots have no vision sensors but" << endl <<
-			"they have an accurate odour sensor that allows them to follow the player!" << endl << endl << endl <<
-			"you can only move to one of the 8 neighbour cells of your current cell." << endl <<
-			"these are the letters you can use to move :" << endl <<
-			"Q           W            E" << endl << endl <<
-			"A          you           D" << endl << endl <<
-			"Z           X            C" << endl << endl <<
-			"you can also stay in the same place by choosing 'S'" << endl << endl;
+		cout << "----------------------------------------------------------------------------------------------------------" << endl <<
+			"|                                                RULES                                                   |" << endl <<
+			"----------------------------------------------------------------------------------------------------------" << endl <<
+			"|You will be placed in a maze made up of high-voltage fences.                                            |" << endl <<
+			"|There are robots that will try to destroy you.                                                          |" << endl <<
+			"|The only way you have to destroy them (and win the game)                                                |" << endl <<
+			"|is to make them crash with each other or make them crash                                                |" << endl <<
+			"|with the eletric fences (sorry, you can not punch them).                                                |" << endl <<
+			"|If you are the one hiting the robots or colliding with the fences you die.                              |" << endl <<
+			"|Every time you make a move the robots will also make a move going to your direction by the shortest path|" << endl <<
+			"|(thats how you trick them). The robots have no vision sensors but                                       |" << endl <<
+			"|they have an accurate odour sensor that allows them to follow the player!                               |" << endl <<
+			"|                                                                                                        |" << endl <<
+			"|                                                                                                        |" << endl <<
+			"|you can only move to one of the 8 neighbour cells of your current cell.                                 |" << endl <<
+			"|these are the letters you can use to move :                                                             |" << endl <<
+			"|                                     Q           W            E                                         |" << endl <<
+			"|                                                                                                        |" << endl <<
+			"|                                     A          you           D                                         |" << endl <<
+			"|                                                                                                        |" << endl <<
+			"|                                     Z           X            C                                         |" << endl <<
+			"|                                                                                                        |" << endl <<
+			"|you can also stay in the same place by choosing 'S'.                                                    |" << endl <<
+			"----------------------------------------------------------------------------------------------------------" << endl;
 		menu();
 	}
 	else if (answer == 2)
@@ -85,7 +94,7 @@ void menu()
 	}
 }
 int main()
-{	
+{
 	testes();
 	menu();
 	return 0;
